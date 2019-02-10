87167c304bdaf1fdaee9808b6f55d2316707f7b3
fcherance <fabien.cherance@gmail.com>
2019-02-10 20:32:40 +0000

    Cleaning names of teams

--- EPL_Predictions_FC.R
+++ EPL_Predictions_FC.R
@@ -54,6 +54,8 @@ EPL_stats$FOR_AVG <- EPL_stats$F / EPL_stats$P
 EPL_stats$AGAINST_AVG <- EPL_stats$A / EPL_stats$P
 EPL_stats$DIFF_AVG <- EPL_stats$GD / EPL_stats$P
 
+EPL_stats <- data.table(EPL_stats)
+
 # EPL Fixtures Predictions ------------------------------------------------
 
 EPL_fixtures_t <- 
@@ -61,47 +63,64 @@ EPL_fixtures %>%
   html_nodes("article") %>%
   html_text()
 
+EPL_fixtures_s <-
+EPL_fixtures_t %>%
+  tstrsplit(":",fixed=TRUE)
 
+fixtures <-data.table(Home=gsub("[0-9]","",unlist(EPL_fixtures_s[[1]])),
+              Away=gsub("[0-9]","",unlist(EPL_fixtures_s[[2]])))
 
-home_team <- EPL_fixtures %>%
-  html_nodes("article") %>%
-  html_text()
-
-away_team <- EPL_fixtures %>%
-  html_nodes(".team-away a") %>%
-  html_text()
-
-fixtures <- data.table(Home=home_team,Away=away_team)
-
-figures <- setdiff(names(EPL_stats),"TEAM.NAME")
+figures <- setdiff(names(EPL_stats),"Team")
 
 fixtures$Home_Goals <- 0
 fixtures$Away_Goals <- 0
 fixtures$Home_Expect <- 0
 fixtures$Away_Expect <- 0
 
+
+LU_ClubNames <- sapply(EPL_stats$Team,
+function(x)grep(x,unique(c(fixtures$Home,fixtures$Away)),value=T)
+)
+
+fixtures$Home<-names(LU_ClubNames[match(fixtures$Home,LU_ClubNames)])
+fixtures$Away<-names(LU_ClubNames[match(fixtures$Away,LU_ClubNames)])
+
+
 for (i in 1:nrow(fixtures)){
-  figdiffs <- EPL_stats[TEAM.NAME==fixtures$Home[i],figures,with=F] - EPL_stats[TEAM.NAME==fixtures$Away[i],figures,with=F]
-  
+
+  figdiffs <- EPL_stats[Team==fixtures$Home[i],figures,with=F] 
+              - 
+              EPL_stats[Team==fixtures$Away[i],figures,with=F]
+
+
   fixtures$Home_Expect[i] <- 
     max(
-      # Expected FOR deteriorated by an opposer's good defence (Away team against goals low)
-      EPL_stats[TEAM.NAME==fixtures$Home[i],FOR_AVG]*(EPL_stats[TEAM.NAME==fixtures$Away[i],AGAINST_AVG]/5)+
-        # The differenciation factor is the gap in goals difference (half applied on Home, half on away)
-        EPL_stats[TEAM.NAME==fixtures$Away[i],AGAINST_AVG]+figdiffs$DIFF_AVG/2,
+      # Expected FOR deteriorated by an opposer's good defence 
+      # (Away team against goals low)
+      EPL_stats[Team==fixtures$Home[i],FOR_AVG]*
+                  (EPL_stats[Team==fixtures$Away[i],AGAINST_AVG]/5)+
+        # The differenciation factor is the gap in goals difference 
+        # (half applied on Home, half on away)
+        EPL_stats[Team==fixtures$Away[i],AGAINST_AVG]+figdiffs$DIFF_AVG/2,
       0)
   
   fixtures$Away_Expect[i] <- 
     max(
       # Formulas are the symmetrical to the ones applied for Home team
-      EPL_stats[TEAM.NAME==fixtures$Away[i],FOR_AVG]*(EPL_stats[TEAM.NAME==fixtures$Home[i],AGAINST_AVG]/5)+
-        EPL_stats[TEAM.NAME==fixtures$Home[i],AGAINST_AVG]-figdiffs$DIFF_AVG/2,
-      0)
+      EPL_stats[Team==fixtures$Away[i],FOR_AVG]*
+        (EPL_stats[Team==fixtures$Home[i],
+                                   AGAINST_AVG]/5)+
+        EPL_stats[Team==fixtures$Home[i],
+                                  AGAINST_AVG]-
+                        figdiffs$DIFF_AVG/2,
+        0)
   
   
   fixtures$Home_Goals[i] <- floor(fixtures$Home_Expect[i])
   fixtures$Away_Goals[i] <- floor(fixtures$Away_Expect[i])
 }
+
+View(fixtures)
 
 write.csv(fixtures[,1:4,with=F],paste("C:\\Users\\Fabien\\Desktop\\EPL_Predictions_FC",
                                       Sys.Date(),".csv"),row.names = F)
